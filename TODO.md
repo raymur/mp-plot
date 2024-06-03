76a39062c6e050b8816d520227d57c54c2f0d5ed
ray <rayem3@gmail.com>
2024-06-02 21:52:07 -0400

    - improve date x axis labeling (done)   - provide key for colors (done)   - update pandas/plot logic in the api (DONE)

---
+++ TODO.md
@@ -0,0 +1,27 @@
+TODOS
+probably a good idea to sort the todos based on priority for MVP
+
+MVP
+ - improve date x axis labeling (done)
+ - provide key for colors (done)
+ - update pandas/plot logic in the api (DONE)
+ - UI improvements
+     - use attractive components & MP theme colors
+     - widen search field
+     - change text on search button
+     - loading icons while waiting for response
+ - use temp file for csvs, dfs, and plots in a data dir
+ - get running in production
+
+
+Subsequent Improvemnts
+ - caching tick files for multiple runs and graph generations
+ - local storage for last used url
+ - smooth transitions for displaying / changing plot
+ - form for more custom graph options (groom more)
+ - best fit line?
+ - improve letter grade normalized rating codes
+ - ability to upload excel file for private ticks
+ - constraints for generating graph
+ - add bouldering and ice 
+ - advertise on MP forums & reddit
--- parse_ticks.py
+++ parse_ticks.py
@@ -1,11 +1,48 @@
+from matplotlib.colors import ListedColormap
+from urllib import request
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy
 import re
-from urllib import request
+import datetime
+import math
 
 TICK_FILENAME = 'ticks.csv'
 PLOT_FILENAME = 'tick_plot.png'
+
+def normailize_rating_code(x):
+    conditions = {
+        800: 1625, # '3rd'
+        900: 1875, # '4th'
+        950: 2000, # 'Easy 5th'
+        1000: 2125, # '5.0'
+        1100: 2375, # '5.1'
+        1200: 2625, # '5.2'
+        1300: 2875, # '5.3'
+        1400: 3125, # '5.4'
+        1500: 3375, # '5.5'
+        1600: 3625, # '5.6'
+        1800: 3875, # '5.7'
+        1900: 3925, # '5.7+'
+        2000: 4075, # '5.8-'
+        2100: 4125, # '5.8'
+        2200: 4175, # '5.8+'
+        2300: 4325, # '5.9-'
+        2400: 4375, # '5.9'
+        2500: 4425} # '5.9+'
+    if abs(3000 - x) <= 500: # 5.10
+        x += 2000
+    elif abs(5000 - x) <= 500: # 5.11
+        x += 1000
+    elif abs(9000 - x) <= 500: # 5.13
+        x -= 1000
+    elif abs(11000 - x) <= 500: # 5.14
+        x -= 2000
+    elif abs(12000 - x) <= 500: # 5.15
+        x -= 2000
+    else:
+        x = conditions.get(x, x)
+    return x
 
 def download_ticks(user_url):
     # sanitize inputs
@@ -24,13 +61,36 @@ def get_tick_df():
     columns = ['Date', 'Rating Code', 'Route Type', 'Pitches', 'Style']
     df = df.loc[df['Style'].isin(style_types), columns]
     df = df.loc[df['Route Type'].str.contains('Trad|Sport', regex=True)]
+    df['Date'] = df['Date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").date() if type(x) == str else x)
     df = df.sort_values(by='Date', ascending=True)
+    df['Normalized Rating Code'] = df['Rating Code'].apply(normailize_rating_code)
     return df
+
+def generate_yticks(ylim_min, ylim_max):
+    yticks = numpy.append(
+        numpy.arange(1625, 4375+1, step=250), 
+        numpy.arange(5000, 10000+1, step=1000))
+    labels = numpy.array(['3rd', '4th', '5.0', '5.1', '5.2', '5.3', '5.4', '5.5', '5.6', '5.7', '5.8', '5.9', 
+            '5.10', '5.11', '5.12', '5.13', '5.14', '5.15'])
+    filter_arr = (ylim_min <=  yticks) & (yticks <= ylim_max)
+    yticks = yticks[filter_arr]
+    labels = list(labels[filter_arr])
+    return yticks, labels
 
 def save_plot(df):
     x = df['Date']
-    y = df['Rating Code']
-    plt.scatter(x, y)
+    y = df['Normalized Rating Code']
+    c = df.apply(lambda x : 3 if (x['Style'] == 'Solo') else (1 if (x['Route Type'] == 'Sport') else 2) , axis=1)
+    area = (df['Pitches'].apply(lambda x : math.pow(x, 0.9)) * 15) 
+    styles = ['Sport', 'Trad', 'Solo']
+    colors = ListedColormap(['r','b','y'])
+    scatter = plt.scatter(x, y, c=c, cmap=colors, s=area, alpha=0.5) 
+    ylim_min, ylim_max = plt.ylim()
+    yticks, ylabels = generate_yticks(ylim_min, ylim_max)
+    plt.yticks(yticks, ylabels)
+    plt.xticks(rotation=45)
+    plt.legend(handles=scatter.legend_elements()[0], labels=styles, loc='upper left', shadow=True)
+    plt.title('Climbing Ticks')
     plt.savefig(PLOT_FILENAME)
     return PLOT_FILENAME
 
